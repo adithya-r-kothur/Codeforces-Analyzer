cc5b277234be725beb6efc3b81e0dfaf0560eec3
adithya <adithya.r.kothur@gmail.com>
2023-02-19 00:20:59 +0530

    updated contest page made it working

--- codeforcesanalyzer/views.py
+++ codeforcesanalyzer/views.py
@@ -7,6 +7,30 @@ import requests
 import random
 
 
+def unixtime_to_indiantime(unixtime):
+    # Convert Unix time to UTC datetime object
+    utc_datetime = dt.datetime.utcfromtimestamp(unixtime)
+
+    # Convert UTC datetime to Indian Standard Time (IST) datetime object
+    ist_offset = dt.timedelta(hours=5, minutes=30) # IST is 5 hours 30 minutes ahead of UTC
+    ist_datetime = utc_datetime + ist_offset
+
+    # Format IST datetime as a string and return it
+    return ist_datetime.strftime('%Y-%m-%d %H:%M:%S')
+def convertUnixTime(unixtime):
+    date = dt.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d')
+    time = dt.datetime.fromtimestamp(unixtime).strftime('%H:%M:%S')
+    date_time_obj = dt.datetime.strptime(date + " " + time, '%Y-%m-%d %H:%M:%S')
+    time = date_time_obj.time()
+    time = str((dt.datetime.combine(dt.date(1, 1, 1), time) +
+                dt.timedelta(hours=5, minutes=30)).time())
+    return date + " " + time
+
+
+def convertToHour(secondsTime):
+    return str(dt.timedelta(seconds=secondsTime))
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -127,4 +151,58 @@ def suggest(request):
 
 
 def contest(request):
-    return render(request, 'contest.html')
+    contestli = requests.get('https://codeforces.com/api/contest.list?gym=false')
+    contestsjson = contestli.json()
+    contestsdata = json.dumps(contestsjson)
+
+    contests = json.loads(contestsdata)
+
+    one = contests['result'][4]
+    one['startTimeSeconds'] = unixtime_to_indiantime(one['startTimeSeconds'])
+    one['durationSeconds'] = convertToHour(one['durationSeconds'])
+
+    two = contests['result'][3]
+    two['startTimeSeconds'] = unixtime_to_indiantime(two['startTimeSeconds'])
+    two['durationSeconds'] = convertToHour(two['durationSeconds'])
+
+    three = contests['result'][2]
+    three['startTimeSeconds'] = unixtime_to_indiantime(three['startTimeSeconds'])
+    three['durationSeconds'] = convertToHour(three['durationSeconds'])
+
+    four = contests['result'][1]
+    four['startTimeSeconds'] = unixtime_to_indiantime(four['startTimeSeconds'])
+    four['durationSeconds'] = convertToHour(four['durationSeconds'])
+
+    five = contests['result'][0]
+    five['startTimeSeconds'] = unixtime_to_indiantime(five['startTimeSeconds'])
+    five['durationSeconds'] = convertToHour(five['durationSeconds'])
+
+    params = {
+
+        'name1': one['name'],
+        'id1': one['id'],
+        'start1': one['startTimeSeconds'],
+        'duration1': one['durationSeconds'],
+
+        'name2': two['name'],
+        'id2': two['id'],
+        'start2': two['startTimeSeconds'],
+        'duration2': two['durationSeconds'],
+
+        'name3': three['name'],
+        'id3': three['id'],
+        'start3': three['startTimeSeconds'],
+        'duration3': three['durationSeconds'],
+
+        'name4': four['name'],
+        'id4': four['id'],
+        'start4': four['startTimeSeconds'],
+        'duration4': four['durationSeconds'],
+
+        'name5': five['name'],
+        'id5': five['id'],
+        'start5': five['startTimeSeconds'],
+        'duration5': five['durationSeconds']
+    }
+
+    return render(request, 'contest.html', params)
